c52b4a0d64007c2aa67a54615fdd986d404b0baa
SignalSin <michaelhenderson.it@gmail.com>
2014-07-11 20:03:31 +0800

    Fixed mouse/touch control. Changed scrolling. Added spawning enemies. Added player bullets.

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ var player;
 var maxVelocity = 150;
 var acceleration = 10;
 var cursors;
-var speed = -1;
+var speed = 1;
 
 var map;
 var layer;
+var bullets;
+var bulletTime = 0;
 
+var enemyPool;
+var nextEnemyAt;
+var enemyDelay;
 
 function preload() {
-    game.load.image('imgPlayer', 'assets/star.png');
-    game.load.tilemap('worldMap', 'assets/tilemap.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('sprites', 'assets/sprites.png');
-    game.load.image('vertBounds', 'assets/vertBounds.png');
-
+    game.load.image('imgPlayer', 'assets/player.png');
+    game.load.image('bullets', 'assets/bullet.png');
+    game.load.image('bgTile', 'assets/Backgrounds/darkPurple.png');
+    game.load.image('enemy', 'assets/enemies/enemyBlack2.png');
 }
 
 function create() {
@@ -26,38 +30,23 @@ function create() {
     //enable keyboard cursors
     cursors = game.input.keyboard.createCursorKeys();
     
-    //create the tilemap
-    map = game.add.tilemap('worldMap');
-    map.addTilesetImage('sprites', 'sprites');
-    layer = map.createLayer('world');
-    
-    layer.resizeWorld();
+    //create the background
+    bgTile = game.add.tileSprite(0, 0, game.stage.bounds.width, game.stage.bounds.height, 'bgTile');
     
     createPlayer();
-    
-    game.camera.y = game.world.height;
-    
-    //set the vertical boundaries
-    topBounds = game.add.sprite(0, -4, 'vertBounds');
-    topBounds.fixedToCamera = true;
-    game.physics.enable(topBounds);
-    topBounds.body.immovable = true;
-    
-    bottomBounds = game.add.sprite(0, 480, 'vertBounds');
-    bottomBounds.fixedToCamera = true;
-    game.physics.enable(bottomBounds);
-    bottomBounds.body.immovable = true;
+    createEnemies();
     
 }
 
 function update(){
-    game.physics.arcade.collide(player, bottomBounds);
-    game.physics.arcade.collide(player, topBounds);
+    bgTile.tilePosition.y += speed;
+    
+    game.physics.arcade.overlap(bullets, enemyPool, enemyHit, null, this );
 
+    
     updatePlayer();
-    
-    game.camera.y += speed;
-    
+    updateEnemies();
+
 }
 
 function createPlayer(){
@@ -66,11 +55,50 @@ function createPlayer(){
     player.body.collideWorldBounds = true;
     player.fixedToCamera = false;
     player.body.gravity.y = 0.1;
+    player.anchor.setTo(0.5, 0.5);
     
+    createBullets();
+}
+
+function createBullets(){
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.createMultiple(30, 'bullets');
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 2);
+}
+
+function createEnemies(){
+    enemyPool = game.add.group();
+    enemyPool.enableBody = true;
+    enemyPool.physicsBodyType = Phaser.Physics.ARCADE;
+    enemyPool.createMultiple(50, 'enemy');
+    enemyPool.setAll('anchor.x', 0.5);
+    enemyPool.setAll('anchor.y', 0.5);
+    enemyPool.setAll('outOfBoundsKill', true);
+    enemyPool.setAll('checkWorldBounds', true);
+    
+    nextEnemyAt = 0;
+    enemyDelay = 1000;
 }
 
 function updatePlayer(){
     playerMovement();
+    fireBullet();
+}
+
+function updateEnemies(){
+    if (nextEnemyAt < game.time.now && enemyPool.countDead() > 0) {
+        nextEnemyAt = game.time.now + enemyDelay;
+        enemy = enemyPool.getFirstExists(false);
+        // spawn at a random location top of the screen
+        enemy.reset(game.rnd.integerInRange(20, 1004), 0);
+        // also randomize the speed
+        enemy.body.velocity.y = game.rnd.integerInRange(30, 60);
+    }
+
 }
 
 function playerMovement(){
@@ -116,8 +144,35 @@ function playerKeyboardMovement(){
 }
 
 function playerMouseMovement(){
-    if (game.input.mousePointer.isDown)  {
-        game.physics.arcade.moveToPointer(player, maxVelocity);
+    
+    if ((game.input.mousePointer.isDown) &&
+        (game.physics.arcade.distanceToPointer(player) > 15)){
+            game.physics.arcade.moveToPointer(player, maxVelocity);
+        
     }
+}
+
+function fireBullet () {
+
+    //  To avoid them being allowed to fire too fast we set a time limit
+    if (game.time.now > bulletTime)
+    {
+        //  Grab the first bullet we can from the pool
+        bullet = bullets.getFirstExists(false);
+        
+        if (bullet)
+        {
+            //  And fire it
+            bullet.reset(player.x, player.y + 8);
+            bullet.body.velocity.y = -400;
+            bulletTime = game.time.now + 700;
+        }
+    }
+
+}
+
+function enemyHit(bullet, enemy){
+    bullet.kill();
+    enemy.kill();
 }
 
